495bfc16911496bc6bb0570a1cedbba441f3088b
AbdielP <abdiel.pinzonc@gmail.com>
2021-08-19 16:05:19 -0500

    basic grid created

--- src/app/pages/qrgenerator/qrgenerator.module.ts
+++ src/app/pages/qrgenerator/qrgenerator.module.ts
@@ -4,11 +4,9 @@ import { CommonModule } from '@angular/common';
 import { QrgeneratorRoutingModule } from './qrgenerator-routing.module';
 import { QraccessComponent } from './qraccess/qraccess.component';
 import { QrcargoComponent } from './qrcargo/qrcargo.component';
-import { QrgeneratorComponent } from './qrgenerator.component';
-
 
 @NgModule({
-  declarations: [QraccessComponent, QrcargoComponent, QrgeneratorComponent],
+  declarations: [QraccessComponent, QrcargoComponent],
   imports: [
     CommonModule,
     QrgeneratorRoutingModule
