6fcc615692b8308ae840ac84c17f0bf9d5f5d695
AbdielP <abdiel.pinzonc@gmail.com>
2021-08-19 03:59:56 -0500

    material module created

---
+++ src/app/materials/materials.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const modules = []
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class MaterialsModule { }
