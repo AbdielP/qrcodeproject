735f5f81e6ba5b12090c411f2482b3b05ceda9e3
AbdielP <abdiel.pinzonc@gmail.com>
2021-08-19 23:52:50 -0500

    search value catched on search.component.ts

--- src/app/pages/qrgenerator/qraccess/search/search.component.ts
+++ src/app/pages/qrgenerator/qraccess/search/search.component.ts
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  value = '';
+  value: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(term: string): void {
+    console.log(term);
   }
 
 }
