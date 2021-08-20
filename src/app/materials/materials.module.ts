edadc144054b3d7ade9fb73f0018ce0d33cae5ae
AbdielP <abdiel.pinzonc@gmail.com>
2021-08-19 23:25:03 -0500

    set media screen properties to qraccess content

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -4,8 +4,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
-const modules = [MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule]
+
+const modules = [MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule]
 
 @NgModule({
   declarations: [],
--- src/app/pages/qrgenerator/qrgenerator.module.ts
+++ src/app/pages/qrgenerator/qrgenerator.module.ts
@@ -1,14 +1,19 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QrgeneratorRoutingModule } from './qrgenerator-routing.module';
 import { QraccessComponent } from './qraccess/qraccess.component';
 import { QrcargoComponent } from './qrcargo/qrcargo.component';
+import { SearchComponent } from './qraccess/search/search.component';
+import { MaterialsModule } from 'src/app/materials/materials.module';
 
 @NgModule({
-  declarations: [QraccessComponent, QrcargoComponent],
+  declarations: [QraccessComponent, QrcargoComponent, SearchComponent],
   imports: [
     CommonModule,
+    MaterialsModule,
+    FormsModule,
     QrgeneratorRoutingModule
   ]
 })
