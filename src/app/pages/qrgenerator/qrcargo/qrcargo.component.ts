a479a7a445a1e429195a5a70792f646395273886
AbdielP <abdiel.pinzonc@gmail.com>
2021-09-11 18:49:48 -0500

    styling download button

--- src/app/pages/qrgenerator/qrcargo/qrcargo.component.ts
+++ src/app/pages/qrgenerator/qrcargo/qrcargo.component.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrcargoComponent implements OnInit {
 
+  data = {
+    plantillaCarga: `ID de Cliente: LOC####
+    Cliente: Nombre_de_la_Empresa_propietaria
+    Responsable: Nombre_de_la_persona_que_entrega_el_articulo
+    Teléfono: ####-#### / ####-####
+    Email: Usuario@DominioDelCliente.com
+    Fecha de Ingreso: dd/mm/aaaa
+    Fecha de Egreso: dd/mm/aaaa
+    SN: X8215467WZ
+    Marca: Dell
+    Modelo: Poweredge 2950
+    Registrado por: Nombre_de_Especialista_IDC`
+  }
   qrcode: string = '';
 
   constructor() { }
