f60f123a0bc1473c53bc1e6a6a7a71095c45f7e9
AbdielP <abdiel.pinzonc@gmail.com>
2021-09-11 20:44:30 -0500

    ngForm and textautosize for textarea

--- src/app/pages/qrgenerator/qrcargo/qrcargo.component.ts
+++ src/app/pages/qrgenerator/qrcargo/qrcargo.component.ts
@@ -1,5 +1,7 @@
 import { NgForm } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild } from '@angular/core';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-qrcargo',
@@ -10,26 +12,34 @@ export class QrcargoComponent implements OnInit {
 
   data = {
     plantillaCarga: `ID de Cliente: LOC####
-    Cliente: Nombre_de_la_Empresa_propietaria
-    Responsable: Nombre_de_la_persona_que_entrega_el_articulo
-    Teléfono: ####-#### / ####-####
-    Email: Usuario@DominioDelCliente.com
-    Fecha de Ingreso: dd/mm/aaaa
-    Fecha de Egreso: dd/mm/aaaa
-    SN: X8215467WZ
-    Marca: Dell
-    Modelo: Poweredge 2950
-    Registrado por: Nombre_de_Especialista_IDC`
+Cliente: Nombre_de_la_Empresa_propietaria
+Responsable: Nombre_de_la_persona_que_entrega_el_articulo
+Teléfono: ####-#### / ####-####
+Email: Usuario@DominioDelCliente.com
+Fecha de Ingreso: dd/mm/aaaa
+Fecha de Egreso: dd/mm/aaaa
+SN: X8215467WZ
+Marca: Dell
+Modelo: Poweredge 2950
+Registrado por: Nombre_de_Especialista_IDC`
   }
   qrcode: string = '';
 
-  constructor() { }
+  constructor(private _ngZone: NgZone) { }
+
+  @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
   ngOnInit(): void {
   }
 
   downloadQR(form: NgForm): void {
     console.log(form);
+  }
+
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this._ngZone.onStable.pipe(take(1))
+        .subscribe(() => this.autosize.resizeToFitContent(true));
   }
 
 }
